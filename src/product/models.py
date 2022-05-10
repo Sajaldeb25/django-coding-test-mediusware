1b62f526d708c3d976b07e2f83c9bfede6443966
Sajal <sajaldebnath02@gmail.com>
2022-05-10 21:53:32 +0600

    Product model updated with fields.

--- src/product/models.py
+++ src/product/models.py
@@ -8,11 +8,15 @@ class Variant(TimeStampMixin):
     description = models.TextField()
     active = models.BooleanField(default=True)
 
+    def __str__(self):
+        return self.title
+
 
 class Product(TimeStampMixin):
     title = models.CharField(max_length=255)
     sku = models.SlugField(max_length=255, unique=True)
     description = models.TextField()
+    created_date = models.DateTimeField(auto_now=True, blank=True)
 
 
 class ProductImage(TimeStampMixin):
@@ -22,8 +26,8 @@ class ProductImage(TimeStampMixin):
 
 class ProductVariant(TimeStampMixin):
     variant_title = models.CharField(max_length=255)
-    variant = models.ForeignKey(Variant, on_delete=models.CASCADE)
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    variant = models.ForeignKey(Variant, on_delete=models.CASCADE, related_name="product_variant")
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name="variants")
 
 
 class ProductVariantPrice(TimeStampMixin):
@@ -35,4 +39,5 @@ class ProductVariantPrice(TimeStampMixin):
                                               related_name='product_variant_three')
     price = models.FloatField()
     stock = models.FloatField()
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name="variant_price")
+
